b43af7c50dc63f279e8b26744983425fddc895da
CoDyung <38072284+snoopingh@users.noreply.github.com>
2023-02-03 17:29:47 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
@@ -7,12 +5,49 @@ class Solution {
         for (int i = 0; i < commands.length; i++) {
             int[] command = commands[i];
             int len = command[1] - command[0] + 1;
-            int[] copyArray = new int[len];
-            System.arraycopy(array, command[0] - 1, copyArray, 0, len);
-            Arrays.sort(copyArray);
-            answer[i] = copyArray[command[2] - 1];
+
+            int[] arr = new int[len];
+            System.arraycopy(array, command[0] - 1, arr, 0, len);
+
+            quickSort(arr, 0, len - 1);
+            answer[i] = arr[command[2] - 1];
         }
 
         return answer;
+    }
+
+    private void quickSort(int[] arr, int start, int end) {
+        int pivotIndex = partition(arr, start, end);
+
+        if (start < pivotIndex - 1) {
+            quickSort(arr, start, pivotIndex - 1);
+        }
+
+        if (pivotIndex < end) {
+            quickSort(arr, pivotIndex, end);
+        }
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+
+        while (start <= end) {
+            while (arr[start] < pivot) start++;
+            while (arr[end] > pivot) end--;
+
+            if (start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+
+        return start;
+    }
+
+    private void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
