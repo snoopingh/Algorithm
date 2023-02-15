5e01f28ddb1b09397e6aed320066b4f43dff7a51
CoDyung <38072284+snoopingh@users.noreply.github.com>
2023-02-15 23:27:10 +0900

    [level 2] Title: 위장, Time: 0.10 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+
+        int answer = 1;
+        
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        
+        return answer - 1;
+    }
+}
