022da5a2be0e7c424ab5b695b71466b41156a60f
CoDyung <38072284+snoopingh@users.noreply.github.com>
2023-02-22 23:42:22 +0900

    [level 2] Title: 가장 큰 수, Time: 2.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : arr) {
+            sb.append(str);
+        }
+        
+        return arr[0].equals("0") ? "0" : sb.toString();
+    }
+}
